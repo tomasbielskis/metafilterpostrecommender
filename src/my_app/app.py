25f81ede4ff45675eb898a505e5f1344edfb853d
Tomas Bielskis <tomasbielskis@gmail.com>
2018-01-18 17:53:41 -0800

    web app

--- src/my_app/app.py
+++ src/my_app/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, render_template
 import pickle
 import pandas as pd
-from build_model import TextClassifier, get_data
 app = Flask(__name__)
 
 # home page
 @app.route('/')
 def index():
-    return render_template('index.html', title='Topic Predictions!')
+    return render_template('index.html', title='Metafilter.com post recommender')
 
 # Form page to submit text
 @app.route('/submission_page/')
@@ -22,12 +21,13 @@ def submission_page():
 # My word counter app
 @app.route('/topic_predictor', methods=['POST'] )
 def topic_predictor():
-    text = str(request.form['user_input'])
-    with open('/home/tomas/galvanize/dsi-data-products/my_app/static/model.pkl', 'rb') as f:
-        model = pickle.load(f)
-    page = 'This is your topic {0}'
-    return page.format(model.predict([text]))
-
+    user_name = str(request.form['user_input'])
+    page = 'These are some posts that you might like: {0}'
+    return page.format(model.predict([user_name]))
 
 if __name__ == '__main__':
+    with open('static/model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+
     app.run(host='0.0.0.0', port=8080, debug=True)
--- src/my_app/model-web.py
+++ src/my_app/model-web.py
@@ -42,7 +42,7 @@ def NMF_Frob(path, tfidf, tfidf_vectorizer):
     return nmf
 
 def sim_rec(user_data, item_data, index, top_n = 10):
-    """Calculates up post recommendations for an individual user
+    """Calculates post recommendations for an individual user
     """
     s = cosine_similarity(user_data[user_data.index==index], item_data).flatten()
     post_indexes = s.argsort()[::-1][0:top_n]
@@ -55,34 +55,30 @@ def user_lookup(user_name, user_data):
 
 if __name__ == '__main__':
     # Setting up the parameters
-    n_features = 50000 #max features used in NMF including unigrams and bigrams
-    n_components = 50 #latent features
-    n_top_words = 20 #for printing model output
-    pool_size = 4 #number of processors on the machine for stemming
+    # n_features = 50000 #max features used in NMF including unigrams and bigrams
+    # n_components = 50 #latent features
+    # n_top_words = 20 #for printing model output
+    # pool_size = 4 #number of processors on the machine for stemming
 
-    #if form input
-    data = #form input
-    data_stemmed = run_stemmer(output_path, data) #change to pd.read_json if already present
-    # Load models
-    model_path = '../data/nlp/train/'
-    tfidf_vectorizer = pickle.load(open(model_path+'tfidfvectorizer', 'rb'))
-    nmf1 = pickle.load(open(model_path+'NMF_Frobenius', 'rb'))
-    tfidf = tfidf_vect(output_path, data_stemmed)
-    user_data = NMF_Frob(output_path, tfidf, tfidf_vectorizer)
-    index = 0
+    # #if form input
+    # data = #form input
+    # data_stemmed = run_stemmer(output_path, data) #change to pd.read_json if already present
+    # # Load models
+    # model_path = '../data/'
+    # tfidf_vectorizer = pickle.load(open(model_path+'tfidfvectorizer', 'rb'))
+    # nmf1 = pickle.load(open(model_path+'NMF_Frobenius', 'rb'))
+    # tfidf = tfidf_vect(output_path, data_stemmed)
+    # user_data = NMF_Frob(output_path, tfidf, tfidf_vectorizer)
+    # index = 0
 
     #if existing user:
-    user_data = pd.read_json('../data/user_data1')#complete user data
+    user_data = pd.read_json('data/user_data1')#complete user data
     dfuser = pd.read_csv('data/usernames.txt',sep='\t', header=1, parse_dates=['joindate'], skiprows=0, index_col='userid')
     user_name = 'Jaclyn' # from form on site
     index = user_lookup(user_name, user_data1)#look up the index of a user
 
 # Available posts to recommend, needs to be prepared outside
     item_test_data1 = pd.read_json(path+'gl_item_test_data1')#syntehtic new posts
-    # item_train_data = pd.read_json(path+'gl_item_train_data1')
-    # item_train_data.set_index('postid',inplace=True)
-    # item_train_data.index.rename(None,inplace=True)
-    # item_train_data.columns = item_train_data.columns.map(int)
     item_data = item_test_data#.append(item_train_data)
 
-    sim_rec(user_data,item_data,index)
+    sim_rec(user_data, item_data, index)
