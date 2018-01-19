5c53095cd498d369e691b8e634825fbf4596d81a
Tomas Bielskis <tomasbielskis@gmail.com>
2018-01-18 21:51:06 -0800

    precision and recall including random recs

--- src/similarity_rec_eval.py
+++ src/similarity_rec_eval.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
@@ -15,27 +16,27 @@ def sim_rec(user_data, item_data, top_n=10):
 
 def metrics(recs, actuals):
     act = {u: list(actuals[actuals['faver']==u]['target']) for u in set(actuals['faver'])}
-    precission = {}
+    precision = {}
     recall = {}
     for u in act.keys():
         tp = len([p for p in act[u] if p in recs[u]])
-        precission[u] = tp / float(len(set(recs[u])))
+        precision[u] = tp / float(len(set(recs[u])))
         recall[u] = tp / float(len(set(act[u])))
-    return (np.mean([i for i in precission.values()])*100,
+    return (np.mean([i for i in precision.values()])*100,
             np.mean([i for i in recall.values()])*100)
 
 if __name__ == '__main__':
-    path = '../data/rec_input/'
+    path = '../data/rec_input/current/'
     user_data = pd.read_json(path+'user_data1')#complete user data
     item_test_data = pd.read_json(path+'gl_item_test_data1')#syntehtic new posts
     fav_posts_test = pd.read_json(path+'gl_fav_posts_test')#actuals to compare
 
-    item_train_data = pd.read_json(path+'gl_item_train_data1')
-    item_train_data.set_index('postid',inplace=True)
-    item_train_data.index.rename(None,inplace=True)
-    item_train_data.columns = item_train_data.columns.map(int)
+    # item_train_data = pd.read_json(path+'gl_item_train_data1')
+    # item_train_data.set_index('postid',inplace=True)
+    # item_train_data.index.rename(None,inplace=True)
+    # item_train_data.columns = item_train_data.columns.map(int)
 
-    fav_posts_train = pd.read_json(path+'gl_fav_posts_train')#actuals to compare
+    # fav_posts_train = pd.read_json(path+'gl_fav_posts_train')#actuals to compare
 
     item_data = item_test_data.append(item_train_data)
     fav_posts = fav_posts_test.append(fav_posts_train)
@@ -43,6 +44,7 @@ if __name__ == '__main__':
     perf = {}
     for n in [5,10,50,100,1000]:
         recs = sim_rec(user_data,item_data, top_n=n)#run the function above or load a file
+        # random_recs = {u: random.sample(set(item_data.index),n) for u in user_data.index}
         perf[n] = metrics(recs, fav_posts)
 
     print(perf)
