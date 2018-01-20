12b5c81c0d15e6404db38d218c2854bb3910c749
Tomas Bielskis <tomasbielskis@gmail.com>
2018-01-19 16:28:56 -0800

    app improved for MVP

--- src/my_app/app.py
+++ src/my_app/app.py
@@ -26,15 +26,17 @@ def submission_page():
         </form>
         '''
 
-# My word counter app
 @app.route('/post_recommender', methods=['POST'] )
 def post_recommender():
     user_string = str(request.form['user_input1'])
-    page = 'These are some posts that you might like: {0}'
-    if len(user_string.split(' ')) == 1:
-        return page.format(recommend_user(user_string))
+    # page = 'These are some posts that you might like: {0}'
+    if user_string == 'test':
+        return 'lame user name, try again'
+    elif len(user_string.split(' ')) == 1:
+        post_dict = recommend_user(user_string)
     else:
-        return page.format(recommend_text(user_string))
+        post_dict = recommend_text(user_string)
+    return render_template('predict.html',posts=post_dict)
 
 def stemming(post):
     """A function for stemming the words in a list of posts"""
@@ -70,12 +72,19 @@ def user_lookup(user_name):
     else:
         return None
 
+def post_lookup(post_list):
+    post_titles = pd.read_csv('data/posttitles_mefi.txt',sep='\t', header=1, skiprows=0, index_col='postid', error_bad_lines=False)
+    return {p: post_titles[post_titles.index==p]['title'].values[0] for p in post_list if len(post_titles[post_titles.index==p]['title']) >= 1}
+
 def recommend_user(user_name):
     index = user_lookup(user_name)#look up the index of a user
     if index != None:
-        return sim_rec(user_data, item_data, index,top_n=10)
-    else:
-        return 'User not found'
+        posts = sim_rec(user_data, item_data, index,top_n=10)
+        pt = post_lookup(posts)
+        if len(pt) >=1:
+            return post_lookup(posts)
+        else:
+            return 'User not found'
 
 def recommend_text(string):
     data_stemmed = stemming(string) #change to pd.read_json if already present
@@ -84,7 +93,8 @@ def recommend_text(string):
     tfidf = tfidf_vect(data_stemmed, tfidf_vectorizer)
     nmf_features = NMF_Kullback(tfidf, nmf2)
     nmf_features = pd.DataFrame(nmf_features)
-    return sim_rec(nmf_features, item_data, 0,top_n=10)
+    postids = sim_rec(nmf_features, item_data, 0,top_n=10)
+    return post_lookup(postids)
 
 if __name__ == '__main__':
     user_data = pd.read_json('data/user_data2')#complete user data
